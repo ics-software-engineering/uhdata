aee0010fc8a2c176df56ce1aea6c51535c79b54b
Cam Moore <cammymoore07@gmail.com>
2023-01-16 09:41:44 -1000

    Create cities.js
    Population data for Hawaii cities.

---
+++ cities.js
@@ -0,0 +1,163 @@
+1	Urban Honolulu	351,554
+2	East Honolulu	50,076
+3	Hilo	46,559
+4	Pearl City	45,941
+5	Kailua CDP	40,402
+6	Waipahu	39,927
+7	Kaneohe	34,509
+8	Ewa Gentry	28,125
+9	Mililani Town	27,974
+10	Kahului	27,938
+11	Kapolei	23,248
+12	Kihei	22,840
+13	Mililani Mauka	21,657
+14	Kailua CDP	21,460
+15	Makakilo	20,862
+16	Schofield Barracks	18,255
+17	Wahiawa	17,357
+18	Ocean Pointe	16,676
+19	Wailuku	15,670
+20	Halawa	14,990
+21	Ewa Beach	14,614
+22	Royal Kunia	14,390
+23	Waianae	14,152
+24	Waimalu	13,818
+25	Lahaina	13,261
+26	Maili	13,245
+27	Nanakuli	12,587
+28	Hawaiian Paradise Park	11,942
+29	Kaneohe Base	11,829
+30	Kaiminani	11,557
+31	Kapaa	11,516
+32	Waihee-Waiehu	11,059
+33	Waipio	10,989
+34	Waimea CDP	10,422
+35	Hickam Housing	10,022
+36	Makaha	9,710
+37	Aiea	9,527
+38	Haiku-Pauwela	9,347
+39	Ahuimanu	8,943
+40	Lihue	8,371
+41	Makawao	7,670
+42	Waikoloa Village	7,519
+43 TIE	Kula	7,495
+43 TIE	Waikele	7,495
+45	Pukalani	7,315
+46	Kahaluu-Keauhou	7,236
+47	Napili-Honokowai	6,762
+48	Ewa Villages	6,657
+49	Wailua Homesteads	6,634
+50	Wailea	6,546
+51	Laie	6,436
+52	West Loch Estate	6,239
+53	Iroquois Point	5,737
+54	Waipio Acres	5,618
+55	Waimanalo	5,371
+56	Kahaluu	5,006
+57	Heeia	4,777
+58	Kalaheo	4,776
+59	Haleiwa	4,651
+60	Hanamaulu	4,435
+61	Pupukea	4,416
+62	Waimanalo Beach	4,389
+63	Hawaiian Beaches	4,213
+64	Puhi	4,154
+65	Captain Cook	4,087
+66	Waikapu	4,002
+67	Ainaloa	3,963
+68	Whitmore Village	3,881
+69	Hauula	3,869
+70	Hawaiian Ocean View	3,851
+71	East Kapolei	3,610
+72	Mountain View	3,467
+73	Helemano	3,404
+74	Kilauea	3,349
+75	Kaunakakai	3,263
+76	Waialua	3,222
+77	Kurtistown	3,115
+78	Hawaiian Acres	3,093
+79	Lanai City	3,079
+80	Kekaha	3,014
+81	Kalaeloa	2,808
+82	Anahola	2,787
+83	Orchidlands Estates	2,749
+84	Lawai	2,733
+85	Holualoa	2,710
+86	Keokea	2,645
+87	Wailua	2,630
+88	Honaunau-Napoopoo	2,538
+89	Hanapepe	2,456
+90	Paia	2,410
+91	Kahuku	2,371
+92	Wheeler AFB	2,353
+93	Koloa	2,348
+94	Eleele	2,329
+95	Pahala	2,210
+96	Kealakekua	2,128
+97	Maunawili	2,071
+98	Kapaau	2,044
+99	Kualapuu	2,024
+100	Princeville	1,969
+101	Honokaa	1,933
+102	Mokuleia	1,886
+103	Waimea CDP	1,863
+104	Fern Acres	1,781
+105	Ko Olina	1,776
+106	Kaloko	1,707
+107	Leilani Estates	1,674
+108	Pepeekeo	1,539
+109	Nanawale Estates	1,538
+110	Kaumakani	1,476
+111	Omao	1,459
+112	Pahoa	1,336
+113	Royal Hawaiian Estates	1,264
+114	Hawi	1,252
+115	Kaaawa	1,231
+116	Keaau	1,188
+117	Kaanapali	1,164
+118	Mahinahina	1,160
+119	Discovery Harbour	1,154
+120	Laupahoehoe	1,099
+121	Haliimaile	1,092
+122	Punaluu	1,062
+123	Volcano Golf Course	1,029
+124	Wainaku	1,020
+125	Poipu	1,008
+126	Olinda	931
+127 TIE	Naalehu	923
+127 TIE	Honalo	923
+129	Mauna Loa Estates	918
+130	Papaikou	873
+131	Fern Forest	787
+132	Waiahole-Waikane	766
+133	Volcano	765
+134 TIE	Halaula	762
+134 TIE	Eden Roc	762
+136	Hana	657
+137	Paukaa	636
+138	Waikoloa Beach Resort	629
+139	Launiupoko	620
+140	Paauilo	469
+141	Hanalei	416
+142	Kukuihaele	393
+143	Ualapue	391
+144	Honomu	390
+145	Kapalua	384
+146	Kawela Bay	369
+147	Tiki Gardens	354
+148	Pakala Village	346
+149	Haena	316
+150	Maunaloa	270
+151	Puako	269
+152	Kalihiwai	260
+153	Maalaea	223
+154	Waiohinu	220
+155	Seaview	217
+156	Wainiha	181
+157 TIE	Makena	167
+157 TIE	Kamaili	167
+159	Makaha Valley	155
+160	Black Sands	143
+161	Olowalu	91
+162	Manele	47
+163	Kalapana	30
